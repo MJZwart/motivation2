6c8f225b317254b0f48f00f2e6e32d7021b79452
Mitchell Zwart <mitchellzwart91@gmail.com>
2025-08-25 15:44:10 +0200

    Added icons for village overview

---
+++ resources/js/village/HappinessIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <component :is="icon" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import HappyIcon from '../components/icons/mood/HappyIcon.vue';
+import GladIcon from '../components/icons/mood/GladIcon.vue';
+import NeutralIcon from '../components/icons/mood/NeutralIcon.vue';
+import SadIcon from '../components/icons/mood/SadIcon.vue';
+import AngryIcon from '../components/icons/mood/AngryIcon.vue';
+
+const {happiness} = defineProps<{happiness: number}>();
+
+const icon = computed(() => {
+    if (happiness >= 70) return HappyIcon;
+    if (happiness >= 60) return GladIcon;
+    if (happiness >= 40) return NeutralIcon;
+    if (happiness >= 20) return SadIcon;
+    return AngryIcon;
+});
+</script>
--- resources/js/village/VillageOverview.vue
+++ resources/js/village/VillageOverview.vue
@@ -1,13 +1,29 @@
 <template>
-    <div panel>
-        <h3 my-2>Village</h3>
-        <div grid cols-2>
-            <b>Inhabitants:</b> 500
-            <b>Stuff:</b> Growing
-            <b>Happiness:</b> Low
+    <div bg-white bg-opacity-3>
+        <div flex justify-center items-center gap-5 @click="collapse = !collapse" cursor-pointer>
+            <h3 my-2>{{ villageName }}</h3>
+            <span flex items-center gap-1><PersonIcon /> 500</span>
+            <span flex items-center gap-1><TrendUpIcon /> Growing</span>
+            <span flex items-center gap-1><HappinessIcon :happiness /> {{happiness}}/100</span>
+        </div>
+        <div 
+            flex 
+            justify-center
+            :class="collapse ? 'max-h-0 opacity-0' : 'max-h-96 opacity-100'"
+            transition="all duration-300 ease-in-out"
+        >
+            This is some more information
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import PersonIcon from '../components/icons/PersonIcon.vue';
+import TrendUpIcon from '../components/icons/TrendUpIcon.vue';
+import HappinessIcon from './HappinessIcon.vue';
+
+const villageName = ref('Village');
+const happiness = ref(85);
+const collapse = ref(true);
 </script>
